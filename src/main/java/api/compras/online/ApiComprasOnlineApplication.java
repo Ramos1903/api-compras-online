e12c6cf04f39b8500446dd06263264ada0ebd922
Vinicius <vinicius.jr08@hotmail.com>
2023-05-13 17:27:03 -0300

    initial commit

---
+++ src/main/java/api/compras/online/ApiComprasOnlineApplication.java
@@ -0,0 +1,13 @@
+package api.compras.online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComprasOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComprasOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/compras/online/ApiComprasOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package api.compras.online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComprasOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
